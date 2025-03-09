db392453947a3c9e2b816b7a442d5c19d4ac2871
raquaza2 <57903323+raquaza2@users.noreply.github.com>
2025-03-10 07:24:15 +0800

    randomNumberGenerator

---
+++ randomNumber/index.js
@@ -0,0 +1,19 @@
+const myButton = document.getElementById('myButton');
+const label1 = document.getElementById('label1');
+const label2 = document.getElementById('label2');
+const label3 = document.getElementById('label3');
+const min = 1;
+const max = 6;
+let randomNumber1;
+let randomNumber2;
+let randomNumber3;
+
+myButton.onclick = function() {
+  randomNumber1 = Math.floor(Math.random() * (max - min + 1)) + min;
+    randomNumber2 = Math.floor(Math.random() * (max - min + 1)) + min;
+    randomNumber3 = Math.floor(Math.random() * (max - min + 1)) + min;
+    label1.textContent = randomNumber1;
+    label2.textContent = randomNumber2;
+    label3.textContent = randomNumber3;
+}
+
